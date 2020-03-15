880430dacf6a4b4bb6a5e3116fb4aa98b5cea36b
wndouglas <wndouglas@hotmail.co.uk>
2020-03-15 11:48:59 +0000

    changes to example file

--- examples/PCFT.cpp
+++ examples/PCFT.cpp
@@ -16,36 +16,61 @@
 using namespace PCFT;
 using namespace std;
 
-void do_transform(string file_name);
+void do_transform(string file_name, int N, int M, double r, double sigma, double T, double Smin, double Smax, double eps1, double eps2);
 void output_to_file(string file_name, const OutputPage& op, const DomainParameters& inputPage, tools::Timer::milliseconds dur, const double r, const double sigma, const double T);
 
 int main(int argc, const char * argv[])
 {
-    std::string file_name = "PCFT_out.txt";
-    if (argc == 2)
+    std::string file_name;
+    int N;
+    int M;
+    double r;
+    double sigma;
+    double T;
+    double Smin;
+    double Smax;
+    double epsilon1;
+    double epsilon2;
+    try
+    {
         file_name = argv[1];
+        N = std::stoi(argv[2]);
+        M = std::stoi(argv[3]);
+        r = std::stod(argv[4]);
+        sigma = std::stod(argv[5]);
+        T = std::stod(argv[6]);
+        Smin = std::stod(argv[7]);
+        Smax = std::stod(argv[8]);
+        epsilon1 = std::stod(argv[9]);
+        epsilon2 = std::stod(argv[10]);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        std::cerr << "Given file name: " << file_name << '\n';
+        std::cerr << "N = " << N << '\n';
+        std::cerr << "M = " << M << '\n';
+        std::cerr << "r = " << r << '\n';
+        std::cerr << "sigma = " << sigma << '\n';
+        std::cerr << "T = " << T << '\n';
+        std::cerr << "Smin = " << Smin << '\n';
+        std::cerr << "Smax = " << Smax << '\n';
+        std::cerr << "epsilon1 = " << epsilon1 << '\n';
+        std::cerr << "epsilon2 = " << epsilon2 << '\n';
+    }
 
-    do_transform(file_name);
+    do_transform(file_name, N, M, r, sigma, T, Smin, Smax, epsilon1, epsilon2);
 
 	return 0;
 }
 
-void do_transform(string file_name)
+void do_transform(string file_name, int N, int M, double r, double sigma, double T, double Smin, double Smax, double eps1, double eps2)
 {
     using namespace numerics;
     using namespace tools;
 
-    int N = 100;
-    int M = 10;
-
-    // // Parameters
-    // double K = 100.0;
-    double r = 0.05;
-    double sigma = 0.2;
-    double T = 1.0;
-
-    double xMin = std::log(0.1);
-    double xMax = std::log(200);
+    double xMin = std::log(Smin);
+    double xMax = std::log(Smax);
 
     // Test usage of preprocessor
     DomainParameters pPackage 
@@ -57,8 +82,8 @@ void do_transform(string file_name)
         xMax,
         T,
 
-        1e-8,
-        1e-8
+        eps1,
+        eps2
     };
     
     PCFTExecutor executor(
@@ -86,22 +111,17 @@ void output_to_file(string file_name,
 {
     ofstream myfile;
     myfile.open (file_name);
-    myfile << "-------------------- Input and parameters --------------------" << std::endl;
-    myfile << "M = " << pPackage.M 
-            << ", N = " << pPackage.N 
-            << ", x_min = " << pPackage.xMin 
-            << ", x_max = " << pPackage.xMax 
-            << std::endl;
-    myfile << "r = " << r << ", sigma = " << sigma << ", T = " << T << std::endl;
-    myfile << std::endl;
 
-    myfile << "-------------------- Output --------------------" << std::endl;
-    myfile << "Calculation time: " << dur << std::endl;
-    myfile << "S, V(S, 0)" << std::endl;
-
+    myfile << "S, V(S, 0), M, N, x_min, x_max, r, sigma, T, CalcTime" << std::endl;
     for (int i = 0; i < pPackage.N; i++)
     {
-        myfile << op.state[i] << ", " << op.valueFunction[0][i] << std::endl;
+        myfile << op.state[i] << ", " << op.valueFunction[0][i];
+        if(i == 0)
+            myfile << ", " << pPackage.M << ", " << pPackage.N << ", " << pPackage.xMin << ", " << pPackage.xMax << ", " << r << ", " << sigma << ", " << T << ", " << dur;
+        else
+            myfile << ", 0, 0, 0, 0, 0, 0, 0, 0";
+
+        myfile << std::endl;
     }
 
     myfile.close();
